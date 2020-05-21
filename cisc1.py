ece8c0dfa4feaa94e845b057e0ef2bad7c6d3c0b
Casey Buchanan <casey.buchanan@edf-re.com>
2020-05-21 08:13:12 -0700

    updating cisc1.py

--- cisc1.py
+++ cisc1.py
@@ -18,10 +18,23 @@ nxos2 = {
     'password':'88newclass',
 }
 
+ios1 = {
+    'device_type':'cisco_ios',
+    'host':'cisco3.lasthop.io',
+    'username':'pyclass',
+    'password':'88newclass',
+}
+
 devices = [nxos1, nxos2]
 
 for dev in devices:
     conn = Netmiko(**dev)
     conn.enable()
-    ver = conn.send_command('show ver')
-    print(ver)
+    cmd = conn.find_prompt()
+    print(cmd)
+
+ios_conn = Netmiko(**ios1)
+ios_conn.enable()
+cmd_ios = ios_conn.send_command('show ver')
+print(cmd_ios, file=open('ios1_ver.txt','w'))
+
