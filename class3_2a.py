db7eadcf829e87b1d34e5a976d624c9bd9a8585a
Casey Buchanan <50712719+cbuch09@users.noreply.github.com>
2020-05-23 13:46:30 -0700

    Create class3_2a.py

---
+++ class3_2a.py
@@ -0,0 +1,8 @@
+devices = [	
+  {'device_name':'cisco3','host':'cisco3.lasthop.io','username':'pyclass','password':'pyclasspass123'},	
+  {'device_name':'cisco4','host':'cisco4.lasthop.io','username':'pyclass','password':'pyclasspass123'},	
+  {'device_name':'arista1','host':'arista1.lasthop.io','username':'pyclass','password':'pyclasspass123'},	
+  {'device_name':'arista2','host':'arista2.lasthop.io','username':'pyclass','password':'pyclasspass123'},	
+  ]	
+
+print(devices[1]['host'])
