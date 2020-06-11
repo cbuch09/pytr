e35794d0ab0bf0959a33833f82305bae1b7e6f9c
Casey Buchanan <casey.buchanan@edf-re.com>
2020-06-10 17:41:45 -0700

    most recent labs

--- class3_2b.py
+++ class3_2b.py
@@ -1,9 +1,10 @@
 import yaml
+from pprint import pprint
 
 devices = []
 
 with open('class3_2b.yml','w') as x:
   yaml.dump(devices, x, default_flow_style=False)
   
-  
-print(devices)
+print(x)
+pprint(devices)
---
+++ class3_json1.py
@@ -0,0 +1,6 @@
+import json
+
+with open('json_interfaces.json') as x:
+    interfaces = json.load(x)
+
+print(interfaces)
