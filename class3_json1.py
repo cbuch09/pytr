8aaeb6722176a1e671dc44abf80b4cf480348dc1
Casey Buchanan <casey.buchanan@edf-re.com>
2020-06-10 18:11:36 -0700

    update with for loop

--- class3_json1.py
+++ class3_json1.py
@@ -3,4 +3,19 @@ import json
 with open('json_interfaces.json') as x:
     interfaces = json.load(x)
 
-print(interfaces)
+#print(interfaces)
+
+v4 = []
+v6 = []
+
+for interfaces, int_dict in interfaces.items():
+    for address, address_dict in int_dict.items():
+        for ip, ip_dict in address_dict.items():
+            prefix_length = ip_dict['prefix_length']
+            if address == 'ipv4':
+                v4.append(f'{ip}, {prefix_length}')
+            if address == 'ipv6':
+                v6.append(f'{ip}, {prefix_length}')
+
+print(f'IPv4 Address: {v4}')
+print(f'IPv6 Address: {v6}') 
