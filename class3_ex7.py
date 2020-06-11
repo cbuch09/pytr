6244ab2000f27db8949ba2a6da0c0e759bb4518d
Casey Buchanan <casey.buchanan@edf-re.com>
2020-06-10 19:06:04 -0700

    add lab

--- class3_ex7.py
+++ class3_ex7.py
@@ -1,6 +1,7 @@
+from ciscoconfparse import CiscoConfParse
 
-
-bgp_output = '''router bgp 44
+bgp_output = '''
+router bgp 44
  bgp router-id 10.220.88.38
  address-family ipv4 unicast
  !
@@ -17,4 +18,19 @@ bgp_output = '''router bgp 44
   address-family ipv4 unicast
    route-policy ALLOW in
    route-policy ALLOW out
-   '''
+'''
+
+bgp_parse = CiscoConfParse(bgp_output.splitlines())
+
+neigh = bgp_parse.find_objects_w_parents(parentspec=r'router bgp',childspec=r'neighbor')
+
+peers = []
+
+for n in neigh:
+    _, neighbor_ip = n.text.split()
+    for child in n.children:
+        if 'remote-as' in child.text:
+            _, asn = child.text.split()
+    peers.append((neighbor_ip, asn))
+
+print(f'Peers: {peers}')
